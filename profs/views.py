3ed46196afb5c1f3bb23b1497d2eec68b8ab413c
Dominik Moritz <domoritz@gmail.com>
2012-07-25 01:28:43 +0200

    added results page

--- profs/views.py
+++ profs/views.py
@@ -1,5 +1,13 @@
 from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.pt')
-def my_view(request):
+def home_view(request):
 	return {'project':'Professors'}
+
+@view_config(route_name='find', renderer='templates/find.pt')
+def find_view(request):
+
+	query_string = request.params['query']
+	query = query_string
+
+	return {'project':'Professors', 'query': query}
