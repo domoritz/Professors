4752ac3f9663f83a3df8c11b53700e0f45d86261
Dominik Moritz <domoritz@gmail.com>
2012-07-25 00:58:03 +0200

    larger search button and input

--- profs/views.py
+++ profs/views.py
@@ -2,4 +2,4 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    return {'project':'Professors'}
+	return {'project':'Professors'}
