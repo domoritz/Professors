c863a76004fa3c8a1a8710ffbd92bf659ffd9183
Dominik Moritz <domoritz@gmail.com>
2012-07-25 12:35:58 +0200

    unit and functional tests

--- profs/tests.py
+++ profs/tests.py
@@ -1,16 +1,55 @@
 import unittest
 
 from pyramid import testing
+from pyramid.testing import DummyRequest
+from pyramid.testing import DummyResource
+from pyramid.compat import url_quote, url_unquote
 
 class ViewTests(unittest.TestCase):
+	def setUp(self):
+		self.config = testing.setUp()
+
+	def tearDown(self):
+		testing.tearDown()
+
+	def test_home_view(self):
+		from .views import home_view
+		request = DummyRequest()
+		response = home_view(request)
+		self.assertEqual(response['project'], 'Professors')
+
+	def test_results_view(self):
+		from .views import results_view
+		request = DummyRequest()
+		q = 'foo bar'
+		request.matchdict = {'query': q}
+		response = results_view(request)
+
+		self.assertEqual(response['query'], q)
+
+
+class FunctionalTests(unittest.TestCase):
     def setUp(self):
-        self.config = testing.setUp()
+        from . import main
 
-    def tearDown(self):
-        testing.tearDown()
+        app = main(None, static_assets = "static")
+        from webtest import TestApp
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'profs')
+        self.testapp = TestApp(app)
+
+    def test_home_page(self):
+        res = self.testapp.get('/', status=200)
+        self.failUnless('id="home"' in res.body)
+
+    def test_redirection_search(self):
+    	q = 'foo bar'
+        res = self.testapp.post('/search?query=foo bar', status=302)
+        self.assertEqual(res.location, 'http://localhost/find/'+url_quote(q))
+
+    def test_results_page(self):
+        res = self.testapp.get('/find/abc', status=200)
+        self.failUnless('id="results"' in res.body)
+
+
+
+
--- profs/views.py
+++ profs/views.py
@@ -1,22 +1,22 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
-from pyramid.compat import url_quote, url_unquote
+
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
 	return {'project':'Professors'}
 
+
 @view_config(route_name='search', request_method='POST')
 def search_view(request):
 	query = request.params['query']
-	query_string = url_quote(query)
-	url = request.route_url('find', query=query_string)
+	url = request.route_url('find', query=query)
 
 	return HTTPFound(location=url)
 
+
 @view_config(route_name='find', renderer='templates/find.pt')
 def results_view(request):
-	query_string = request.matchdict['query']
-	query = url_unquote(query_string)
+	query = request.matchdict['query']
 
-	return {'project':'Professors', 'query': query}
+	return {'query': query}
