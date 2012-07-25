a5a2669be9e2759e480a3b68db66036e6843b1f0
Dominik Moritz <domoritz@gmail.com>
2012-07-25 11:30:32 +0200

    search view and nice search urls

--- profs/__init__.py
+++ profs/__init__.py
@@ -4,10 +4,11 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view(name = settings["static_assets"], path = 'profs:static', cache_max_age=3600)
 
     config.add_route('home', '/')
-    config.add_route('find', '/find')
+    config.add_route('search', '/search')
+    config.add_route('find', '/find/{query}')
     
     config.scan()
     return config.make_wsgi_app()
--- profs/views.py
+++ profs/views.py
@@ -1,13 +1,22 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.compat import url_quote, url_unquote
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
 	return {'project':'Professors'}
 
-@view_config(route_name='find', renderer='templates/find.pt')
-def find_view(request):
+@view_config(route_name='search', request_method='POST')
+def search_view(request):
+	query = request.params['query']
+	query_string = url_quote(query)
+	url = request.route_url('find', query=query_string)
 
-	query_string = request.params['query']
-	query = query_string
+	return HTTPFound(location=url)
+
+@view_config(route_name='find', renderer='templates/find.pt')
+def results_view(request):
+	query_string = request.matchdict['query']
+	query = url_unquote(query_string)
 
 	return {'project':'Professors', 'query': query}
