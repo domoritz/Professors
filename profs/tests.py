96d72573a8a2aa7719fb6c90d807e835b27b01c5
Dominik Moritz <domoritz@gmail.com>
2012-07-27 00:30:00 +0200

    basic popit api access

--- profs/tests.py
+++ profs/tests.py
@@ -1,9 +1,29 @@
 import unittest
 
+from oktest import ok, test
+
 from pyramid import testing
 from pyramid.testing import DummyRequest
 from pyramid.testing import DummyResource
 from pyramid.compat import url_quote, url_unquote
+
+# uses oktest http://www.kuwata-lab.com/oktest/oktest-py_users-guide.html
+
+# import for testing
+from views import *
+
+class UnitTests(unittest.TestCase):
+	@test("query parser parses popit id")
+	def _(self):
+		qp = QueryParser()
+		parsed = qp.parse('id:23h4jk345')
+		ok(parsed) == {'id':['23h4jk345']}
+
+	@test("query parser parses multiple popit ids")
+	def _(self):
+		qp = QueryParser()
+		parsed = qp.parse('id:23h4jk345 id:hsdfsdfwe34')
+		ok(parsed) == {'id':['23h4jk345', 'hsdfsdfwe34']}
 
 class ViewTests(unittest.TestCase):
 	def setUp(self):
@@ -12,48 +32,50 @@ class ViewTests(unittest.TestCase):
 	def tearDown(self):
 		testing.tearDown()
 
-	def test_home_view(self):
+	@test("home view")
+	def _(self):
 		from .views import home_view
 		request = DummyRequest()
 		response = home_view(request)
-		self.assertEqual(response['project'], 'Professors')
+		ok(response['project']) == 'Professors'
 
-	def test_results_view(self):
+	@test("results view")
+	def _(self):
 		from .views import results_view
 		request = DummyRequest()
 		q = 'foo bar'
 		request.matchdict = {'query': q}
 		response = results_view(request)
 
-		self.assertEqual(response['query'], q)
+		ok(response['query']) == q
 
 
 class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from . import main
+	def setUp(self):
+		from . import main
 
-        app = main(None, static_assets = "static")
-        from webtest import TestApp
+		app = main(None, static_assets = "static")
+		from webtest import TestApp
 
-        self.testapp = TestApp(app)
+		self.testapp = TestApp(app)
 
-    def test_unexisting_page(self):
-        res = self.testapp.get('/SomePage', status=404)
-        self.assertTrue('Not Found' in res.body)
+	@test("not existing page returns 404")
+	def _(self):
+		res = self.testapp.get('/SomePage', status=404)
+		ok('Not Found').in_(res.body)
 
-    def test_home_page(self):
-        res = self.testapp.get('/', status=200)
-        self.failUnless('id="home"' in res.body)
+	@test("home page returns 200")
+	def _(self):
+		res = self.testapp.get('/', status=200)
+		ok('id="home"').in_(res.body)
 
-    def test_redirection_search(self):
-    	q = 'foo bar'
-        res = self.testapp.post('/search?query=foo bar', status=302)
-        self.assertEqual(res.location, 'http://localhost/find/'+url_quote(q))
+	@test("search page gets redirected")
+	def _(self):
+		q = 'foo bar'
+		res = self.testapp.post('/search?query=foo bar', status=302)
+		ok(res.location) == 'http://localhost/find/'+url_quote(q)
 
-    def test_results_page(self):
-        res = self.testapp.get('/find/abc', status=200)
-        self.failUnless('id="results"' in res.body)
-
-
-
-
+	@test("results page returns 200")
+	def _(self):
+		res = self.testapp.get('/find/abc', status=200)
+		ok('id="results"').in_(res.body)
--- profs/views.py
+++ profs/views.py
@@ -1,6 +1,9 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from libs.popit.popit import PopIt
+import re
+
+api = PopIt(instance = 'professors', hostname = '127-0-0-1.org.uk', port = 3000, user = 'test@test.co.uk', password = 'tJo1zBum')
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
@@ -19,4 +22,37 @@ def search_view(request):
 def results_view(request):
 	query = request.matchdict['query']
 
+	qp = QueryParser()
+	parsed = qp.parse(query)
+
+	if parsed.has_key('id'):
+		id = parsed['id'][0]
+		results = api.person(id).get()
+
 	return {'query': query}
+
+
+class QueryParser(object):
+	"""docstring for QueryParser"""
+	def __init__(self):
+		pass
+
+	def parse(self, query):
+		self.query = query
+		tokens = query.split()
+
+		parsed = {}
+		for token in tokens:
+			key, value = self.parse_token(token)
+			if not parsed.has_key(key):
+				parsed[key] = []
+			parsed[key].append(value)
+
+		return parsed
+
+	def parse_token(self, token):
+		m=re.match('(.+):(.+)', token)
+		if m:
+			return m.group(1), m.group(2)
+
+		return 'word', token
